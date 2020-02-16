e38364b1ed988b63cc277f74a34b0f1cea8e2082
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-02-16 15:02:16 -0300

    Adição das paaginas de calculadora, uma com lógica usando if e a outra switch case.

---
+++ 2020_02_16/impar_par.php
@@ -0,0 +1,29 @@
+<?php
+setlocale(LC_ALL, "pt-BR");
+?>
+
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <title>Exercicios</title>
+        <meta charset="utf-8">
+        
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">        
+    </head>
+    
+    <body>
+        <?php
+            require_once("modulos/menu.php");
+        ?>
+        
+        <div id="containerPagina">
+            
+        </div>
+        
+        <?php
+            require_once("modulos/footer.php");
+        ?>
+    </body>
+</html>
---
+++ 2020_02_16/modulos/menu.php
@@ -0,0 +1,33 @@
+<?php
+    echo('
+        <nav id="navMenu">
+            <div id="containerMenu">
+                <ul id="menu">
+                    <li class="menuItem">
+                        <a href="#">
+                            <div class="menuText">Média</div>
+                        </a>
+                    </li>
+
+                    <li class="menuItem">
+                        <a href="#">
+                            <div class="menuText">Calculadora</div>
+                        </a>
+                    </li>
+
+                    <li class="menuItem">
+                        <a href="#">
+                            <div class="menuText">Tabuada</div>
+                        </a>
+                    </li>
+
+                    <li class="menuItem">
+                        <a href="#">
+                            <div class="menuText">Par E Impar</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    ');
+?>
