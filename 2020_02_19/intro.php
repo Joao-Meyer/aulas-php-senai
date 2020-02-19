f8fc1bcfd8f8b96ed70d2b0bfe71705da641e19b
JOÃO HENRIQUE PARIZOTI MEYER <19267271@CFP127ALUNO.LOCAL>
2020-02-19 11:24:28 -0300

    Primeiro commit.

---
+++ 2020_02_19/intro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Doug Funni's Burgers</title>
+    
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div id="corpoDaPagina">
+        <?php
+            require_once('modulos/menu.php');
+        ?>
+    </div>  
+</body>
+</html>
