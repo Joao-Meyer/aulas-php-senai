7177cfe0099312661634d6f8efe0377cf0fd48ea
JOÃO HENRIQUE PARIZOTI MEYER <19267271@CFP127ALUNO.LOCAL>
2020-03-02 11:28:28 -0300

    Continuidade do desenvolvimento da página HOME.

--- 2020_02_16/modulos/constantes.php
+++
@@ -1,6 +0,0 @@
-<?php
-    const ERRO_CAIXA_VAZIA = "<span class='mensagensDeErro'>Por favor, inserir dados válidos nas caixas de texto, não é possível realizar cálculos vazios!</span>";
-    const ERRO_CARACTERE_INVALIDO = "<span class='mensagensDeErro'>Por favor, inserir números válidos.</span>";
-    const ERRO_OPERACAO_NAO_DEFINIDA = "<span class='mensagensDeErro'>Por favor, selecione uma operação.</span>";
-    const ERRO_NUMERO_NAO_DEFINIDO = "<span class='mensagensDeErro'>Por favor, selecione ambos os números.</span>"
-?>
