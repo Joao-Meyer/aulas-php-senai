c2cad77a31f263f234286f51ebce657cadee55e1
JOÃO HENRIQUE PARIZOTI MEYER <19267271@CFP127ALUNO.LOCAL>
2020-03-06 16:56:46 -0300

    Atualização da pagina home.

---
+++ 2020_03_06/modulos/conteudoHome.php
@@ -0,0 +1,38 @@
+<div id="secaoConteudo">
+    <div id="conteinerConteudoPagina">
+        <div id="conteinerSlider"></div>
+
+        <div id="conteinerRedesSociais"></div>
+
+        <div id="menuLateral">
+            <div class="menuLateralItem">Item</div>
+            <div class="menuLateralItem">Item</div>
+            <div class="menuLateralItem">Item</div>
+            <div class="menuLateralItem">Item</div>
+        </div>
+
+        <div id="conteinerProdutos">
+            <div class="cardProduto">
+                <figure class="imagemCard">
+                    <img src="./imagens/hamburguer-fresco.jpg" alt="">
+                </figure>
+
+                <div class="textoCard">
+                    <div class="textoCardInformacoes">
+                        <ul>
+                            <li>Nome:</li>
+
+                            <li>Descrição:</li>
+
+                            <li>Preço:</li>
+                        </ul>
+                    </div>
+
+                    <div class="textoCardDetalhes">
+                        Detalhes:
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ 2020_03_06/modulos/menu.php
@@ -0,0 +1,47 @@
+<div id="secaoCabecalho">
+    <div id="conteinerCabecalho">
+        <div id="logo"></div>
+
+        <nav id="menu">
+            <ul>
+                <li class="menuItem">Menu</li>
+
+                <li class="menuItem">Menu</li>
+
+                <li class="menuItem">Menu</li>
+
+                <li class="menuItem">Menu</li>
+
+                <li class="menuItem">Menu</li>
+
+                <li class="menuItem">Menu</li>
+
+                <li class="menuItem">Menu</li>
+            </ul>
+        </nav>
+
+        <div id="campoDeLogin">
+            <form action="" method="post">
+                <div id="containerUsuario">
+                    <div id="textoUsuario">Usuário:</div>
+
+                    <div id="campoUsuario"></div>
+                </div>
+
+                <div id="containerSenha">
+                    <div id="textoSenha">Senha:</div>
+
+                    <div id="campoSenha">
+                        <input type="password">
+                    </div>
+                </div>
+
+                <div class="conteinerBotao">
+                    <button type="submit" class="botaoSubmit">OK</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div style="width: 100%; height: 80px;"></div>
