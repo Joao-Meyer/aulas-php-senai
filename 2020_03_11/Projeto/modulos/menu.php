d622e3c40bc8c19ac45524d6a4bf7ea6d90077de
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-03-11 11:10:04 -0300

    Continuação da aula de responsividade.

---
+++ 2020_03_11/Projeto/modulos/menu.php
@@ -0,0 +1,62 @@
+<div id="secaoCabecalho">
+    <div id="conteinerCabecalho">
+        
+        <a href="home.php">
+            <div id="logo"></div>
+        </a>
+
+        <nav id="menu">
+            <ul>
+                <a href="sobre.php">
+                    <li class="menuItem">HONKER</li>
+                </a>
+                
+                <a href="promocoes.php">
+                    <li class="menuItem">PROMOÇÕES</li>
+                </a>
+                
+                <a href="destaques.php">
+                    <li class="menuItem">DESTAQUES</li>
+                </a>
+
+                <a href="curiosidades.php">
+                    <li class="menuItem">CURIOSIDADES</li>
+                </a>
+
+                <a href="lojas.php">
+                    <li class="menuItem">LOJAS</li>
+                </a>
+
+                <a href="contato.php">
+                    <li class="menuItem">CONTATO</li>
+                </a>
+            </ul>
+        </nav>
+
+        <div id="campoDeLogin">
+            <form action="" method="post">
+                <div id="containerUsuario">
+                    <div id="textoUsuario">Usuário:</div>
+
+                    <div id="campoUsuario">
+                        <input type="text">
+                    </div>
+                </div>
+
+                <div id="containerSenha">
+                    <div id="textoSenha">Senha:</div>
+
+                    <div id="campoSenha">
+                        <input type="password">
+                    </div>
+                </div>
+
+                <div class="conteinerBotao">
+                    <button type="submit" class="botaoSubmit">OK</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div style="width: 100%; height: 80px;"></div>
