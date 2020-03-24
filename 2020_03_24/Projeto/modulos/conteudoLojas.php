2f689885d6983fc6966c87c8bdcb557e5da7c217
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-03-24 18:26:03 -0300

    Modularização dos imports e início do desenvolvimento do slider.

---
+++ 2020_03_24/Projeto/modulos/conteudoLojas.php
@@ -0,0 +1,37 @@
+<div id="secaoConteudo">
+    <div id="conteinerLojas">
+        <h1>NOSSAS LOJAS</h1>
+
+        <div class="conteinerCardLoja">
+            <div class="conteinerInformacaoCardLoja">
+                <h2 class="nomeDaLoja">Honker Burguer</h2>
+
+                <h2 class="enderecoDaLoja">Rua Daora, 123, SP</h2>
+
+                <p class="descricaoDaLoja">Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+                Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+                </p>
+            </div>
+
+            <div class="conteinerImagemCardLoja">
+                <img src="./imagens/lanchonete_doug_funny.jpg" alt="Imagem da loja">
+            </div>
+        </div>
+
+        <div class="conteinerCardLoja">
+            <div class="conteinerInformacaoCardLoja">
+                <h2 class="nomeDaLoja">Honker Burguer</h2>
+
+                <h2 class="enderecoDaLoja">Rua Daora, 123, SP</h2>
+
+                <p class="descricaoDaLoja">Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+                Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+                </p>
+            </div>
+
+            <div class="conteinerImagemCardLoja">
+                <img src="./imagens/lanchonete_doug_funny.jpg" alt="Imagem da loja">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ 2020_03_24/Projeto/modulos/imports.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="shortcut icon" href="./imagens/doug_funny.jpg">
+<script src="./javascript/jimcarey.js"></script>
