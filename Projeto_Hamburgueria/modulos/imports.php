acc92662a18d0dcdd168682a59e7cb7baa57b650
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-04-06 17:13:53 -0300

    Adição do Slider sem botões próximo e anterior.

--- Projeto_Hamburgueria/modulos/imports.php
+++ Projeto_Hamburgueria/modulos/imports.php
@@ -2,4 +2,5 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="shortcut icon" href="./imagens/doug_funny.jpg">
 <script src="./javascript/jimcarey.js"></script>
+<script src="./javascript/cycle.js"></script>
 <script src="./javascript/slide.js"></script>
--- Projeto_Hamburgueria/modulos/slider.php
+++ Projeto_Hamburgueria/modulos/slider.php
@@ -1,5 +1,10 @@
 <div id="conteinerSlider">
-    <div id="previousSlide">&laquo;</div>
+    <ul>
+        <li><img src="./imagens/imagem_slider_1.jpg" alt="Imagem 1 slider" class="imagensSlider"></li>
+        <li><img src="./imagens/imagem_slider_2.jpg" alt="Imagem 2 slider" class="imagensSlider"></li>
+        <li><img src="./imagens/lanchonete_doug_funny.jpg" alt="Imagem 1 slider" class="imagensSlider"></li>
+    </ul>
+    <!-- <div id="previousSlide">&laquo;</div>
 
-    <div id="nextSlide">&raquo;</div>
+    <div id="nextSlide">&raquo;</div> -->
 </div>
