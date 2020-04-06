6d86d6499a0a289441a862fd4b6fa3eede54596b
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-04-06 15:32:38 -0300

    Adição do projeto em uma única pasta

---
+++ Projeto_Hamburgueria/modulos/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div id="conteinerSistemaInterno"> Sistema Interno </div>
+
+    <div id="conteinerEndereco"> Endereço xxxxxxxx-xxxxxxxxxxxxx-xxxxxx-xxxxxxx </div>
+
+    <div id="conteinerApp">
+        <div id="conteinerImagemDoApp"> Imagem </div>    
+    
+        <div id="conteinerLinkDoApp"> Link do app </div>
+    </div>
+</footer>
---
+++ Projeto_Hamburgueria/modulos/imports.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="shortcut icon" href="./imagens/doug_funny.jpg">
+<script src="./javascript/jimcarey.js"></script>
+<script src="./javascript/slide.js"></script>
