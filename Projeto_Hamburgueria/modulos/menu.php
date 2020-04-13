f8b68e4e951c9de960f1819512167feb69b0dcee
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-04-13 20:27:55 -0300

    Inicio dos ajustes de responsividade

--- Projeto_Hamburgueria/modulos/menu.php
+++ Projeto_Hamburgueria/modulos/menu.php
@@ -1,5 +1,37 @@
 <div id="secaoCabecalho">
     <div id="conteinerCabecalho">
+
+        <nav id="conteinerMenuMobile">
+            <div id="iconeMenuMobile"></div>
+
+            <div id="menuMobile">
+                <ul>
+                    <a href="sobre.php">
+                        <li class="menuItemMobile">HONKER</li>
+                    </a>
+                    
+                    <a href="promocoes.php">
+                        <li class="menuItemMobile">PROMOÇÕES</li>
+                    </a>
+                    
+                    <a href="destaques.php">
+                        <li class="menuItemMobile">DESTAQUES</li>
+                    </a>
+
+                    <a href="curiosidades.php">
+                        <li class="menuItemMobile">CURIOSIDADES</li>
+                    </a>
+
+                    <a href="lojas.php">
+                        <li class="menuItemMobile">LOJAS</li>
+                    </a>
+
+                    <a href="contato.php">
+                        <li class="menuItemMobile">CONTATO</li>
+                    </a>
+                </ul>
+            </div>
+        </nav>
         
         <a href="super.php">
             <div id="logo"></div>
