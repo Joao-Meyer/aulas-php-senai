00bc6ba7b6bc91ce0135bffc73dbdf436ae8be44
Joao-Meyer <joaoparizotimeyer@gmail.com>
2020-07-15 10:18:24 -0300

    Alterações visuais na página principal e footer.

--- Projeto_Hamburgueria2.0/modulos/footer.php
+++ Projeto_Hamburgueria2.0/modulos/footer.php
@@ -6,7 +6,7 @@
     <div id="conteinerEndereco"> Endereço xxxxxxxx-xxxxxxxxxxxxx </div>
 
     <div id="conteinerApp">
-        <div id="conteinerImagemDoApp"> Imagem </div>    
+        <div id="conteinerImagemDoApp"></div>    
     
         <div id="conteinerLinkDoApp"> Link do app </div>
     </div>
